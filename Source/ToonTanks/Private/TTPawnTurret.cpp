6d5488ec9cc6de267ebec05f8c3f361790a6160c
Jonathan Villegas <jonathan.e.villegas@gmail.com>
2020-09-15 13:31:06 -0700

    Pawn turret now gets the PlayerPawn and casts to the PawnTank before using (valid check)

--- Source/ToonTanks/Private/TTPawnTurret.cpp
+++ Source/ToonTanks/Private/TTPawnTurret.cpp
@@ -2,4 +2,42 @@
 
 
 #include "TTPawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+#include "ToonTanks/Pawns/TTPawnTank.h"
 
+void ATTPawnTurret::CheckFireCondition()
+{
+	// Player must be valid and not dead.
+	if (PawnTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Pawn is valid!~"));
+	}
+
+	// Check if the player is in range and if so, fire
+
+}
+
+void ATTPawnTurret::InitializePawnTank()
+{
+	PawnTank = Cast<ATTPawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (PawnTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Pawn initialized!"));
+	}
+}
+
+void ATTPawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATTPawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	FireRate = 2.f;
+	InitializePawnTank();
+// 	if (GetWorld())
+// 	{
+		GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &ATTPawnTurret::CheckFireCondition, FireRate, true);
+	//}
+}
--- Source/ToonTanks/Public/TTPawnTurret.h
+++ Source/ToonTanks/Public/TTPawnTurret.h
@@ -14,4 +14,28 @@ class TOONTANKS_API ATTPawnTurret : public ATTPawnBase
 {
 	GENERATED_BODY()
 	
+private:
+
+	
+	UFUNCTION()
+	void CheckFireCondition();
+
+	UFUNCTION()
+	void InitializePawnTank();
+
+	FTimerHandle FireRateTimerHandle;
+
+	class ATTPawnTank* PawnTank;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float FireRate;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 };
